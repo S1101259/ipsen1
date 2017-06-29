c2c8002830d911c32958b2ca2d17e08907b01e7c
Testman3 <testman3@hotmail.com>
2017-06-29 21:15:34 +0200

    Revert "Removed Unused Clutter"
    This reverts commit 52613a69963b6cc5aa0476d1b4dea6c4dd072e01.

---
+++ Research/Henk/Testproject/src/testing/Main.java
@@ -0,0 +1,15 @@
+package testing;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ArrayList<String> alString = new ArrayList<String>();
+		
+		String test = "lol";
+		String test2 = "lol";
+
+	}
+
+}
